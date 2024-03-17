c213823b7514d1d195f3ac4496062eef4c4fc8b5
Alan Anaya Araujo <hello@alananaya.dev>
2024-03-17 01:31:59 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/dev/alananaya/movies_gallery/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.alananaya.movies_gallery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MovieGalleryApp());
+}
+
+class MovieGalleryApp extends StatelessWidget {
+  const MovieGalleryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = "Movies Gallery";
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title, style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.black87,
+        ), 
+        body: Container(
+          color: Colors.black87,
+          child: GridView.count(
+            crossAxisCount: 2,
+            childAspectRatio: 2/3,
+            children: List.generate(9, (index) =>
+                Card(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.white60
+                    ),
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  elevation: 1,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.black87,
+                  borderOnForeground: true,
+                  shadowColor: Colors.white,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage('https://m.media-amazon.com/images/M/MV5BM2QyNDIzOGMtNThhNS00NmUwLWI0ZjUtZjdkN2I1OTRjZWQ3XkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg'),
+                              fit: BoxFit.cover
+                            ),
+                            borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(12)
+                            ),
+                          ),
+                          child: Container(
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                    bottom: 0,
+                                    right: 0,
+                                    left: 0,
+                                    child: Container(
+                                      padding: EdgeInsets.all(5.0),
+                                      child: ClipRect(
+                                        child: BackdropFilter(
+                                          filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
+                                          child: Container(
+                                            height: 50,
+                                            color: Colors.black54,
+                                            child: Padding(
+                                              padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                                              child: Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                    "Drama, Action, Adventure",
+                                                    style: TextStyle(
+                                                      color: Colors.pinkAccent,
+                                                      fontSize: 10
+                                                    ),
+                                                  ),
+                                                  Padding(
+                                                    padding: const EdgeInsets.only(top:8.0),
+                                                    child: Row(
+                                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                                      children: [
+                                                        Row(
+                                                          children:
+                                                            List.generate(
+                                                                5,
+                                                                (index) => Icon(
+                                                                  index < 3 ? Icons.star : Icons.star_border_outlined,
+                                                                  color: index < 3 ? Colors.pink : Colors.grey,
+                                                                  size: 15,
+                                                                )
+                                                            ),
+
+                                                        ),
+                                                        SizedBox(width: 7,),
+                                                        Text(
+                                                          "120 reviews",
+                                                          style: TextStyle(
+                                                            color: Colors.grey,
+                                                            fontSize: 10
+                                                          ),
+                                                        )
+                                                      ],
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                )
+                              ],
+                            )
+                          )
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Mr. Robot $index',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white
+                              ),
+                            ),
+                            Text(
+                                "120 MIN",
+                              style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.grey
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
